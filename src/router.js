19fd0ee9c9c544fd97c19a1c15d0ab8c7c456d1b
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-07 11:36:15 -0500

    add children routes

--- src/router.js
+++ src/router.js
@@ -6,7 +6,14 @@ import {
   import App from "./App"
   
   const router = createBrowserRouter(
-    createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+    createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Index/>}/>
+      <Route path=":id" element={<Show/>}/>
+      <Route path="create"/>
+      <Route path="update/:id"/>
+      <Route path="delete/:id"/>
+    </Route>)
   )
   
   export default router
