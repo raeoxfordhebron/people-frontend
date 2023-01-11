0f8d148052587f736890d6926c2f814587eaf3cf
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-10 19:44:26 -0500

    populate Show component

--- src/loaders.js
+++ src/loaders.js
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom"
+
 const URL = "https://people-backend.onrender.com"
 
 export const peopleLoader = async () => {
     const response = await fetch(URL + "/people")
     const people = await response.json()
     return people
+}
+
+export const personLoader = async ({params}) => {
+    const response = await fetch(URL + "/people/" + params.id)
+    const person = await response.json()
+    return person
 }
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,5 +1,13 @@
+import {useLoaderData} from "react-router-dom"
+
 function Show (props) {
-    return <h1>This is the Show Component</h1>
+    const person = useLoaderData()
+    
+    return <div className="person">
+            <h1>{person.name}</h1>
+            <h2>{person.title}</h2>
+            <img src={person.image} alt={person.name}/>
+    </div>
 }
 
 export default Show
