750520dd4af67c818b79dd5d9fd91dcba12d8588
Rae Hebron <rae.oxford.hebron@gmail.com>
2023-01-10 20:07:14 -0500

    show route fixed

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -6,7 +6,7 @@ function Show (props) {
     return <div className="person">
             <h1>{person.name}</h1>
             <h2>{person.title}</h2>
-            <img src={person.image} alt={person.name}/>
+            <img src={person.image} alt={person.name}></img>
     </div>
 }
 
